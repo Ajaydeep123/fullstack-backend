1342f03a0ae918ac1f2e4453f7a5ef30b1910561
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-02-04 19:38:50 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import {app} from "./app.js"
 dotenv.config({
-    path:"./.env"
+    path:"./.env"    //with this we also make change to the package.json file too
 })
 
 
