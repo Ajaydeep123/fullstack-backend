5d6143554ddd5408fd28d93a77bc72bcf21bb567
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-02-14 23:58:09 +0530

    📝 .

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,11 +1,12 @@
 import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+//model for storing video data
 
 const videoSchema = new Schema(
     {
         videoFile: {
-            type: String, //cloudinary url
+            type: String, //cloudinary url 
             required: true
         },
         thumbnail: {
