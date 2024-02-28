e455593663fee93e70784b2d38fa2a952562939f
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-02-28 20:07:01 +0530

    deleteVideobyId controller implemented

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,10 +1,13 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Video} from "../models/video.model.js"
+import {Like} from "../models/like.model.js"
+import {Comment} from "../models/comment.model.js"
+import {PlayList} from "../models/playlist.model.js"
 import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {uploadOnCloudinary, deleteOnCloudinary, getPublicId} from "../utils/cloudinary.js"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
@@ -20,6 +23,21 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if(!videoId || !isValidObjectId(videoId)){
+        throw new ApiError(400,"This video id is not valid!")
+    }
+
+    const video = await Video.findById({
+        _id:videoId
+    })
+
+    if( !video || ( !video?.isPublished &&  !(video?.owner.toString() === req.user?._id.toString()) ) ){
+        throw new ApiError(404,"Video not found")
+    }
+    
+    return res
+    .status(200)
+    .json(new ApiResponse(200,video,"Video Fetched Successfully!"))
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
@@ -31,6 +49,55 @@ const updateVideo = asyncHandler(async (req, res) => {
 const deleteVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: delete video
+    if(!videoId || !isValidObjectId(videoId)){
+        throw new ApiError(400,"This video id is not valid!")
+    }
+
+    const video = await Video.findById({
+        _id:videoId
+    })
+
+    if(!video){
+        throw new ApiError(404, "video not found")
+    }
+
+    // owner field in the document is objectId of the person who uploaded the video
+    // We'll check whether the current user's _id is same as Owner's objectId, if not then they are not authorized to access that video
+    if(video?.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "Unauthorized: You are not allowed to delete this video");
+    }
+
+    //We'll delete the files from cloudinary and then remove the corresponding document from our collections
+
+    if(video?.videoFile){
+        const videoPublicId = getPublicId(video.videoFile)
+        await deleteOnCloudinary(videoPublicId,"video")
+    }
+
+    if(video?.thumbnail){
+        const thumbnailPublicId = getPublicId(video.thumbnail)
+        await deleteOnCloudinary(thumbnailPublicId, "image")
+    }
+
+    //delete video and associated relations from db
+    await Promise.all([
+        Like.deleteMany({video:videoId}),
+        Comment.deleteMany({video:videoId}),
+        PlayList.updateMany({},{$pull:{videos:videoId}}),
+        User.updateMany({},{$pull:{watchHistory: videoId}})
+    ])
+
+    const deleteResponse = await Video.findByIdAndDelete(videoId);
+
+    if(!deleteResponse){
+        throw new ApiError(500, "something went wrong while deleting video !!")
+    }
+
+    return res.
+    status(200)
+    .json(
+        new ApiResponse(200, deleteResponse, "Video deleted successfully")
+    )
 })
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,6 @@
-/* 
-Approach for file uploading, We'll use multer to get client to upload the file and store it temporarily in public/temp, 
-once the file is uploaded to cloudinary we'll delete it using fs.unlinkSync()
-*/
+ //Approach for file uploading, We'll use multer to get client to upload the file and store it temporarily in public/temp, 
+//once the file is uploaded to cloudinary we'll delete it using fs.unlinkSync()
+
 
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
@@ -32,4 +31,29 @@ const uploadOnCloudinary = async (localFilePath) =>{
     }
 }
 
-export { uploadOnCloudinary }
+// A cloudinary assest url looks like this: https://res.cloudinary.com/<cloud_name>/<resource_type>/<delivery_type>[/<transformations>]/<version>/<public_id>
+//So to perform updates and delete files on cloudinary we'll have to extract the public_id from the url itself
+
+const getPublicId = (assetUrl) =>{
+    const parts = assetUrl.split("/")
+    const assetPart = parts[parts.length-1] 
+    const publicId = assetPart.split(".")[0] //splitting and selecting only the public id not the extension
+    return publicId
+}
+
+const deleteOnCloudinary = async (public_id, resource_type)=>{
+    try {
+        if(!public_id) return null
+    
+        const response = await cloudinary.uploader.destroy(public_id,{
+            resource_type
+        })
+        console.log("file deleted successfully :", response.result)
+        return response
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+export { uploadOnCloudinary,deleteOnCloudinary, getPublicId }
