edee2ff3f6716bf264b3232c23fa77bfcdb301b2
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-03-08 03:51:30 +0530

    ✅ deleteComment controller done

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -21,6 +21,29 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params;
+    if (!commentId || !isValidObjectId(commentId)) {
+        throw new ApiError(400, "commentId is required or invalid");
+    }
+    try {
+        const comment = await Comment.findById(commentId)
+        if(comment?.owner.toString() !== req.user?._id.toString()){
+            throw new ApiError(401, "You are not authorized to delete the comment!")
+        }
+        
+        const deletedComment = await Comment.findByIdAndDelete(commentId);
+        if (!deletedComment) {
+            throw new ApiError(500, "Unable to delete the comment");
+        }
+        return res
+            .status(200)
+            .json(
+            new ApiResponse(200, deletedComment || "Comment deleted successfully")
+            );
+    } catch (error) {
+        throw new ApiError(500, error?.message || "Unable to delete comment");        
+    }
+
 })
 
 export {
