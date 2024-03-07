7d03bb0e9330af8881a3de1f4d842f92521e0b4a
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-03-07 19:25:20 +0530

    ✅ toggle subscription

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -8,12 +8,48 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    const userId = req.user?._id;
     // TODO: toggle subscription
+    if(!channelId || !isValidObjectId(channelId)){
+        throw new ApiError(400, "Channel Id is required");
+    }
+    const filterOrCredential = {subscriber:userId, channel:channelId }
+    try {
+
+        const channel = await User.findById(channelId)
+        if(!channel){
+            throw new ApiError(400,"This channel does not Exists")
+        }            
+        const subscribed = await Subscription.findOne(filterOrCredential);
+        
+        if(!subscribed){
+            const subscribe = await Subscription.create(filterOrCredential);
+            if(!subscribe){
+                throw new ApiError(500, "failed to subscribe the channel")
+            }
+
+            return res.
+            status(200).
+            json(new ApiResponse(200, subscribe, "Successfully subscribed to this channel!"))
+        }else{
+            const unsubscribe = await Subscription.findOneAndDelete(filterOrCredential)
+            if(!unsubscribe){
+                throw new ApiError(500, "failed to unsubscribe the channel")
+            }
+
+            return res.
+            status(200).
+            json(new ApiResponse(200, unsubscribe, "Successfully unsubscribed to this channel!"))
+        }
+    } catch (error) {
+        throw new ApiError(500, error?.message||"Failed to toggle subscription")
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
 })
 
 // controller to return channel list to which user has subscribed
